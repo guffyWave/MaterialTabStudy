6b3eb74577fc29c3efaae49d2664ae5e17356607
gufran <gufran.xpertsinfosoft@gmail.com>
2015-10-17 17:03:01 +0530

    First Commit

---
+++ app/src/main/java/com/appxperts/materialtabsstudy/MainActivity.java
@@ -0,0 +1,27 @@
+package com.appxperts.materialtabsstudy;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appxperts.materialtabsstudy.adapter.DesignDemoPagerAdapter;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        DesignDemoPagerAdapter adapter = new DesignDemoPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+}
